0ec08d49e6bc720f77fd784d7481f8bb5faeedfd
LunaLycan287 <1161223+LunaLycan287@users.noreply.github.com>
2024-08-02 19:47:08 +0200

    Add project files.

---
+++ ChangeBackgroundPerk/ChangeBackgroundPerk.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GimmeMinerBack
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class ChangeBackgroundPerk : BaseUnityPlugin
+    {
+        public const string pluginGuid = "com.github.lunalycan287.starvalormods.changebgperk";
+        public const string pluginName = "Gimme Miner Back";
+        public const string pluginVersion = "1.0.0";
+
+        private static ConfigEntry<int> perkId;
+        private static bool ran = false;
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(ChangeBackgroundPerk));
+            LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
+            perkId = Config.Bind<int>("setting", "perkId", 1, "Perk that should be set.\n" +
+                "Valid Values: \n" +
+                " 0 = Outis; 1 = Miner; 2 = Trader; 3 = White Collar; 4 = Lone Wolf; 5 = Pirate; 6 = Rebel; 7 = Indoctrinated; 8 = Scoundrel");
+        }
+
+
+        [HarmonyPatch(typeof(PerksPanel), "ShowCharPerks")]
+        [HarmonyPrefix]
+        private static void PerksPanel_ShowCharPerks_Pre()
+        {
+            if (ran || PChar.Char.perks == null || !PChar.HasPerk(perkId.Value))
+            {
+                return;
+            }
+
+            PerkDB.AcquirePerk(perkId.Value);
+
+            ran = true;
+        }
+    }
+}
