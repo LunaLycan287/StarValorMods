c8ba49cc7bfb194a9fe9ea08ff32924a1120d718
LunaLycan287 <1161223+LunaLycan287@users.noreply.github.com>
2024-08-03 05:31:34 +0200

    Initial Release MoreSkillPoints

---
+++ MoreSkillPoints/MoreSkillPoints.cs
@@ -0,0 +1,87 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LL_SV_MoreSkillPoints
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class MoreSkillPoints : BaseUnityPlugin
+    {
+        public const string pluginGuid = "lunalycan287.starvalormods.moreskillpoints";
+        public const string pluginName = "More Skill Points";
+        public const string pluginVersion = "1.0.0";
+
+        static ManualLogSource logSource = new ManualLogSource("MoreSkillPoints");
+
+        private static ConfigEntry<int> maxSkillPoints;
+        private static ConfigEntry<bool> ignoreResetPoints;
+        private static ConfigEntry<bool> uninstall;
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(MoreSkillPoints));
+            LoadConfig();
+            BepInEx.Logging.Logger.Sources.Add(logSource);
+        }
+
+        private void LoadConfig()
+        {
+            maxSkillPoints = Config.Bind<int>("General Settings", "MaxSkillPoints", 125, "The amount of skill points at max level.");
+            ignoreResetPoints = Config.Bind<bool>("General Settings", "InfiniteResets", false, "If resseting should not cost any reset points.");
+            uninstall = Config.Bind<bool>("Uninstall", "Uninstall", false, "If this is set to true, changes by this mod will be reset. \n" +
+                "After opening your skill screen and saving your game you will have normal skill points again.");
+        }
+
+        [HarmonyPatch(typeof(PlayerCharacter), "LevelUP")]
+        [HarmonyPostfix]
+        private static void PlayerCharacter_LevelUP_Post()
+        {
+            SetSkillPoints();
+        }
+
+        [HarmonyPatch(typeof(PlayerCharacter), "ResetSkills")]
+        [HarmonyPrefix]
+        private static void PlayerCharacter_ResetSkills_Pre(PlayerCharacter __instance)
+        {
+            if (ignoreResetPoints.Value || uninstall.Value)
+            {
+                __instance.resetSkillsPoints++;
+            }
+        }
+        
+        [HarmonyPatch(typeof(PlayerCharacter), "ResetSkills")]
+        [HarmonyPostfix]
+        private static void PlayerCharacter_ResetSkills_Post()
+        {
+            SetSkillPoints();
+        }
+
+        [HarmonyPatch(typeof(CharacterScreen), "ShowData")]
+        [HarmonyPostfix]
+        private static void CharacterScreen_ShowData_Post()
+        {
+            SetSkillPoints();
+        }
+
+        private static void SetSkillPoints()
+        {
+            if (uninstall.Value)
+            {
+                return;
+            }
+
+            PlayerCharacter player = PChar.Char;
+
+            float maxSP = (float)maxSkillPoints.Value / PChar.maxLevel;
+            float curSkillPoints = maxSP * (float)player.level;
+            int usedSP = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                usedSP += player.GetSkillTreeLevel(i);
+            }
+            player.skillPoints = (int)curSkillPoints - usedSP;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -11,3 +11,22 @@ Change your current background Perk to another one.
 - Set your perk in `Star Valor\BepInEx\config\lunalycan287.starvalormods.changebgperk`
 - Load your save
 - Open the Perk screen with `P` and the Perk will be added
+
+## More Skill Points
+Adjust how many skill points you get during a normal game.
+
+### Install
+- Copy `LL_SV_MoreSkillPoints.dll` into `Star Valor\BepInEx\Plugins`
+- Start the Game
+- Set the ammount of Skill Points in `Star Valor\BepInEx\config\lunalycan287.starvalormods.changebgperk` (Default: 125 = enough for all)
+- Load your save
+- Open the Character screen with `C` and the Skill Points will be adjusted.
+
+### Uninstall
+- Set the value of `uninstall = true` in  `Star Valor\BepInEx\config\lunalycan287.starvalormods.changebgperk` 
+- Start the Game
+- Load your save
+- Open the Character screen with `C` and the Skill Points will be adjusted.
+- Click on `Reset Skills`
+- Save & quit your game
+- Remove the mod
