38c54e6e258857aec49c9303a184ec7f0bc79b1c
LunaLycan287 <1161223+LunaLycan287@users.noreply.github.com>
2025-07-16 20:13:05 +0200

    Added predefined CrewMember & Weapon Dump and Code Refactoring

--- Dumper/Dumper.cs
+++ Dumper/Dumper.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace LL_SV_Dumper {
@@ -35,61 +37,100 @@ namespace LL_SV_Dumper {
         }
 
         public void Update() {
-            if (Input.GetKeyDown(_keybind.Value)) {
-                Directory.CreateDirectory(DumpFolder);
-                foreach (FileInfo file in new DirectoryInfo(DumpFolder).GetFiles())
-                {
-                    file.Delete(); 
-                }
-                /*DumpCrew();*/
-                DumpEquipment();
-                DumpFactions();
-                DumpItems();
-                DumpPerks();
-                DumpShips();
-                DumpSkills();
-                DumpBaseBuilding();
+            if (!Input.GetKeyDown(_keybind.Value)) return;
+            
+            Directory.CreateDirectory(DumpFolder);
+            foreach (FileInfo file in new DirectoryInfo(DumpFolder).GetFiles()) {
+                file.Delete(); 
             }
-        }
-
-        private static void SaveFile(string filename, string content) {
-            string path = Path.Combine(DumpFolder, filename + ".dump.cs");
-            File.WriteAllText(path, content);
-            LOGSource.LogInfo(filename +" Dumped to: " + Path.GetFullPath(path));
+            DumpCrew();
+            DumpEquipment();
+            DumpFactions();
+            DumpItems();
+            DumpPerks();
+            DumpShips();
+            DumpSkills();
+            DumpBaseBuilding();
+            DumpWeapons();
         }
 
         private static string SanitizeName(string name) {
             string cleanName = name.Replace(" ", "_").Replace(".", "_").Replace("'", "");
             cleanName = cleanName.Replace("(Clone)", "").Replace("-", "_").Replace(":", "").Replace("+","_plus");
-            //Crew Only
-            //cleanName = cleanName.Replace("’", "").Replace(",", "");
+            // Crew specific
+            cleanName = cleanName.Replace(",", "");
+
+            if (cleanName.Length == 0) {
+                cleanName = "Unknown";
+            }
             
-            if (Char.IsDigit(cleanName[0])) {
+            if (char.IsDigit(cleanName[0])) {
                 cleanName = "_"+cleanName;
             }
 
             return cleanName;
         }
 
-        /*private static void DumpCrew() {
-            LOGSource.LogInfo("Started CrewMembers Dump");
-            List<CrewMember> crewMembersList = AccessTools.StaticFieldRefAccess<List<CrewMember>>(typeof(CrewDB), "crewList");
-            StringBuilder csb =  new StringBuilder("using System.ComponentModel;");
-            csb.AppendLine();
-            csb.AppendLine("namespace LL_SV_Dumper {");
-            csb.AppendLine("    public enum CrewMembers {");
-            foreach (CrewMember member in crewMembersList) {
-                csb.Append("        [Description(\"").Append(member.aiChar.name).AppendLine("\")]");
-                csb.Append("        ").Append(sanitizeName(member.aiChar.name)).Append(" = ").Append(member.id).AppendLine(",");
-            }
-            csb.AppendLine("    }");
-            csb.AppendLine("}");
-            
-            saveFile("CrewMembers", csb.ToString());
-        }*/
+        private static string SanitizeCrewName(string name) {
+            return name.Replace("’", "'").Replace("\"", "'");
+        }
 
-        private static void DumpEquipment() {
-            LOGSource.LogInfo("Started Equipments Dump");
+        private static StringBuilder DumpStart(string enumName) {
+            StringBuilder sb =  new StringBuilder("using System.ComponentModel;");
+            sb.AppendLine();
+            sb.AppendLine("namespace LL_SV_Dumper {");
+            sb.Append("    public enum ").Append(enumName).AppendLine(" {");
+            return sb;
+        }
+
+        private static void DumpType(StringBuilder sb, string type, int typeId) {
+            sb.Append("        // --- ").Append(type).Append("(").Append(typeId).AppendLine(")");
+        }
+
+        private static void DumpDefaultEntry(StringBuilder sb, string name, int id) {
+            DumpSanitizedEntry(sb, name, SanitizeName(name), id);
+        }
+
+        private static void DumpSanitizedEntry(StringBuilder sb, string name, string sanitizedName, int id, [CanBeNull] string type = null, bool containsDuplicates = false) {
+            sb.Append("        [Description(\"").Append(name);
+            if (type != null) {
+                sb.Append(" (").Append(type).Append(")");
+            }
+            sb.AppendLine("\")]");
+
+            sb.Append("        ").Append(sanitizedName);
+            if (containsDuplicates) {
+                sb.Append("_").Append(id);
+            }
+            sb.Append(" = ").Append(id).AppendLine(",");
+        }
+        
+        private static void DumpEnd(string enumName, StringBuilder sb) {
+            sb.AppendLine("    }");
+            sb.AppendLine("}");
+            
+            string path = Path.Combine(DumpFolder, enumName + ".dump.cs");
+            File.WriteAllText(path, sb.ToString());
+            LOGSource.LogInfo(enumName +" Dumped to: " + Path.GetFullPath(path));
+        }
+
+        private static void DumpCrew() {
+            const string enumName = "CrewMembers";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
+            
+            List<CrewMember> crewMembersList = new List<CrewMember>(GameManager.predefinitions.crewMembers);
+
+            StringBuilder sb = DumpStart(enumName);
+            foreach (CrewMember member in crewMembersList) {
+                DumpDefaultEntry(sb, SanitizeCrewName(member.aiChar.name), member.id);
+            }
+            DumpEnd(enumName, sb);
+        }
+
+        private static void DumpEquipment() {            
+            const string enumName = "Equipments";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
+            
             List<Equipment> equipmentList = AccessTools.StaticFieldRefAccess<List<Equipment>>(typeof(EquipmentDB), "equipments");
             Dictionary<EquipmentType, List<Equipment>> equipmentSorted = new Dictionary<EquipmentType, List<Equipment>>();
             List<EquipmentType> equipmentTypes = new List<EquipmentType>();
@@ -102,72 +143,64 @@ namespace LL_SV_Dumper {
                     equipmentSorted.Add(equipment.type, new List<Equipment>() { equipment });
                 }
             }
-            
-            
-            StringBuilder esb =  new StringBuilder("using System.ComponentModel;");
-            esb.AppendLine();
-            esb.AppendLine("namespace LL_SV_Dumper {");
-            esb.AppendLine("    public enum Equipments {");
+
+
+            StringBuilder sb = DumpStart(enumName);
             foreach (EquipmentType type in equipmentTypes) {
-                esb.Append("        // --- ").Append(type).Append("(").Append((int)type).AppendLine(")");
+                DumpType(sb, type.ToString(), (int) type);
                 equipmentSorted[type].Sort((item, item1) => item.id < item1.id ? -1 : 1);
                 foreach (Equipment equipment in equipmentSorted[type]) {
-                    esb.Append("        //").AppendLine(equipment.name);
-                    esb.Append("        [Description(\"").Append(equipment.equipName).Append(" (").Append(equipment.type).AppendLine(")\")]");
-                    string name = SanitizeName(equipment.equipName);
-                    if (equipment.id == 188 || equipment.id == 189) {
-                        name += "_builtin";
-                    } else if (equipment.id == 182) {
-                        name += "_CoT";
+                    sb.Append("        //").AppendLine(equipment.name);
+                    
+                    string sanitizedName = SanitizeName(equipment.equipName);
+                    // Special cases where we have duplicate name entries.
+                    switch (equipment.id) {
+                        case 188: // Attack Drone MK2 vs Attack Drone MK2 Built In
+                        case 189: // Repair Drone MK2 vs Repair Drone MK2 Built In
+                            sanitizedName += "_builtin";
+                            break;
+                        case 182: //Cloaking vs CloakingCoT aka Terran Mantle
+                            sanitizedName += "_CoT";
+                            break;
                     }
-                    esb.Append("        ").Append(name).Append(" = ").Append(equipment.id).AppendLine(",");
+                    DumpSanitizedEntry(sb, equipment.name, sanitizedName, equipment.id, type.ToString());
                 }
-                esb.AppendLine();
+                sb.AppendLine();
             }
-            
-            esb.AppendLine("    }");
-            esb.AppendLine("}");
-            
-            SaveFile("Equipments", esb.ToString());
+            DumpEnd(enumName, sb);
         }
         
         private static void DumpFactions() {
-            LOGSource.LogInfo("Started Faction Dump");
-            List<Faction> factionList = AccessTools.StaticFieldRefAccess<List<Faction>>(typeof(FactionDB), "factions");
-            StringBuilder fsb =  new StringBuilder("using System.ComponentModel;");
-            fsb.AppendLine();
-            fsb.AppendLine("namespace LL_SV_Dumper {");
-            fsb.AppendLine("    public enum Factions {");
-            foreach (Faction faction in factionList) {
-                fsb.Append("        [Description(\"").Append(faction.factionName).AppendLine("\")]");
-                fsb.Append("        ").Append(SanitizeName(faction.factionName)).Append(" = ").Append(faction.id).AppendLine(",");
-            }
-            fsb.AppendLine("    }");
-            fsb.AppendLine("}");
+            const string enumName = "Factions";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
             
-            SaveFile("Factions", fsb.ToString());
+            List<Faction> factionList = AccessTools.StaticFieldRefAccess<List<Faction>>(typeof(FactionDB), "factions");
+            
+            StringBuilder sb = DumpStart(enumName);
+            foreach (Faction faction in factionList) {
+                DumpDefaultEntry(sb, faction.factionName, faction.id);
+            }
+            DumpEnd(enumName, sb);
         }
 
         private static void DumpItems() {
-            LOGSource.LogInfo("Started Items Dump");
+            const string enumName = "Items";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
+            
             List<Item> itemList = AccessTools.StaticFieldRefAccess<List<Item>>(typeof(ItemDB), "items");
             itemList.Sort((item, item1) => item.id < item1.id ? -1 : 1);
-            StringBuilder isb =  new StringBuilder("using System.ComponentModel;");
-            isb.AppendLine();
-            isb.AppendLine("namespace LL_SV_Dumper {");
-            isb.AppendLine("    public enum Items {");
+
+            StringBuilder sb = DumpStart(enumName);
             foreach (Item item in itemList) {
-                isb.Append("        [Description(\"").Append(item.itemName).AppendLine("\")]");
-                isb.Append("        ").Append(SanitizeName(item.itemName)).Append(" = ").Append(item.id).AppendLine(",");
+                DumpDefaultEntry(sb, item.itemName, item.id);
             }
-            isb.AppendLine("    }");
-            isb.AppendLine("}");
-            
-            SaveFile("Items", isb.ToString());
+            DumpEnd(enumName, sb);
         }
 
         private static void DumpPerks() {
-            LOGSource.LogInfo("Started Perks Dump");
+            const string enumName = "Perks";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
+            
             Dictionary<PerkType, List<Perk>> perkSorted = new Dictionary<PerkType, List<Perk>>();
             List<Perk> perkList = PerkDB.GetAllPerks();
             List<PerkType> perkTypes = new List<PerkType>();
@@ -181,82 +214,85 @@ namespace LL_SV_Dumper {
                 }
             }
 
-            StringBuilder psb = new StringBuilder("using System.ComponentModel;");
-            psb.AppendLine();
-            psb.AppendLine("namespace LL_SV_Dumper {");
-            psb.AppendLine("    public enum Perks {");
+            StringBuilder sb = DumpStart(enumName);
 
             foreach (PerkType type in perkTypes) {
-                psb.Append("        // --- ").Append(type).Append("(").Append((int)type).AppendLine(")");
+                DumpType(sb, type.ToString(), (int) type);
                 foreach (Perk perk in perkSorted[type]) {
-                    psb.Append("        [Description(\"").Append(perk.Name()).Append(" (").Append(perk.Type()).AppendLine(")\")]");
-                    psb.Append("        ").Append(SanitizeName(perk.Name())).Append(" = ").Append(perk.id).AppendLine(",");
+                    DumpSanitizedEntry(sb, perk.Name(), SanitizeName(perk.Name()), perk.id, type.ToString());
                 }
-                psb.AppendLine();
+                sb.AppendLine();
             }
-            
-            psb.AppendLine("    }");
-            psb.AppendLine("}");
-            
-            SaveFile("Perks", psb.ToString());
+            DumpEnd(enumName, sb);
         }
 
         private static void DumpShips() {
-            LOGSource.LogInfo("Started ShipModels Dump");
-            List<ShipModelData> shipsList = AccessTools.StaticFieldRefAccess<List<ShipModelData>>(typeof(ShipDB), "shipModels");
-            StringBuilder smsb =  new StringBuilder("using System.ComponentModel;");
-            smsb.AppendLine();
-            smsb.AppendLine("namespace LL_SV_Dumper {");
-            smsb.AppendLine("    public enum ShipModels {");
-            foreach (ShipModelData ship in shipsList) {
-                smsb.Append("        [Description(\"").Append(ship.shipModelName).AppendLine("\")]");
-                smsb.Append("        ").Append(SanitizeName(ship.shipModelName)).Append(" = ").Append(ship.id).AppendLine(",");
-            }
-            smsb.AppendLine("    }");
-            smsb.AppendLine("}");
+            const string enumName = "ShipModels";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
             
-            SaveFile("ShipModels", smsb.ToString());
-
+            List<ShipModelData> shipsList = AccessTools.StaticFieldRefAccess<List<ShipModelData>>(typeof(ShipDB), "shipModels");
+            
+            StringBuilder sb = DumpStart(enumName);
+            foreach (ShipModelData ship in shipsList) {
+                DumpDefaultEntry(sb, ship.shipModelName, ship.id);
+            }
+            DumpEnd(enumName, sb);
         }
 
         private static void DumpSkills() {
-            LOGSource.LogInfo("Started Skills Dump");
-            List<Skill> skillList = SkillDB.Skills;
-            StringBuilder ssb =  new StringBuilder("using System.ComponentModel;");
-            ssb.AppendLine();
-            ssb.AppendLine("namespace LL_SV_Dumper {");
-            ssb.AppendLine("    public enum Skills {");
-            foreach (Skill skill in skillList) {
-                ssb.Append("        [Description(\"").Append(skill.skillName).AppendLine("\")]");
-                ssb.Append("        ").Append(SanitizeName(skill.skillName)).Append(" = ").Append(skill.id).AppendLine(",");
-            }
-            ssb.AppendLine("    }");
-            ssb.AppendLine("}");
+            const string enumName = "Skills";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
             
-            SaveFile("Skills", ssb.ToString());
+            List<Skill> skillList = SkillDB.Skills;
+            
+            StringBuilder sb = DumpStart(enumName);
+            foreach (Skill skill in skillList) {
+                DumpDefaultEntry(sb, skill.skillName, skill.id);
+            }
+            DumpEnd(enumName, sb);
         }
 
         private static void DumpBaseBuilding() {
-            LOGSource.LogInfo("Started BuildingPlan Dump");
-            List<BuildingPlan> planList = AccessTools.StaticFieldRefAccess<List<BuildingPlan>>(typeof(BaseBuildingDB), "plans");
-            planList.Sort((item, item1) => item.id < item1.id ? -1 : 1);
-            StringBuilder bbsb =  new StringBuilder("using System.ComponentModel;");
-            bbsb.AppendLine();
-            bbsb.AppendLine("namespace LL_SV_Dumper {");
-            bbsb.AppendLine("    public enum BuildingPlans {");
-            foreach (BuildingPlan plan in planList) {
-                if (!String.IsNullOrEmpty(plan.Description)) {
-                    bbsb.Append("       //").AppendLine(plan.Description);
-                }
-
-                bbsb.Append("       [Description(\"").Append(plan.Name).AppendLine("\")]");
-                bbsb.Append("       ").Append(SanitizeName(plan.Name)).Append("_").Append(plan.id).Append(" = ").Append(plan.id).AppendLine(",");
+            const string enumName = "BuildingPlans";
+            if (!GameManager.instance.inGame) {
+                LOGSource.LogWarning("Skipping " + enumName + " Dump. We currently have no game loaded.");
+                return;
             }
-            bbsb.AppendLine("    }");
-            bbsb.AppendLine("}");
+             
+            LOGSource.LogInfo("Started " + enumName + " Dump");
             
+            List<BuildingPlan> planList = AccessTools.StaticFieldRefAccess<List<BuildingPlan>>(typeof(BaseBuildingDB), "plans");
+            //List<BuildingPlan> planList = TODO: Fix plans to load when game not loaded
+            planList.Sort((item, item1) => item.id < item1.id ? -1 : 1);
             
-            SaveFile("BuildingPlans", bbsb.ToString());
+            StringBuilder sb  = DumpStart(enumName);
+            foreach (BuildingPlan plan in planList) {
+                if (!string.IsNullOrEmpty(plan.Description)) {
+                    sb.Append("       //").AppendLine(plan.Description);
+                }
+                
+                DumpSanitizedEntry(sb, plan.Name, SanitizeName(plan.Name), plan.id, null, true);
+            }
+            DumpEnd(enumName, sb);
+        }
+
+        private static void DumpWeapons() {
+            const string enumName = "Weapons";
+            LOGSource.LogInfo("Started " + enumName + " Dump");
+            
+            Dictionary<int, TWeapon> weaponReduced = new Dictionary<int, TWeapon>();
+            List<TWeapon> weaponList = new List<TWeapon>(GameManager.predefinitions.weapons);
+            foreach (TWeapon weapon in weaponList.Where(weapon => !weaponReduced.ContainsKey(weapon.index))) {
+                weaponReduced[weapon.index] = weapon;
+            }
+            
+            StringBuilder sb = DumpStart(enumName);
+            sb.AppendLine("        // All weapons are available in different Weapon Types but the ID stays the same");
+            sb.AppendLine();
+            foreach (TWeapon weapon in weaponReduced.Select(weaponPair => weaponPair.Value)) {
+                DumpDefaultEntry(sb, weapon.name, weapon.index);
+            }
+            DumpEnd(enumName, sb);
         }
     }
 }
